ad623eebdb0a13d4e77510d130415910a1a0a225
Pedro77h <pedrohenriquearaujosoares25@gmail.com>
2022-08-06 23:46:40 -0300

    Aprendendo React Native - Fundamentos RN

--- meuprimeiroapp/App.js
+++ meuprimeiroapp/App.js
@@ -5,17 +5,43 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.outtitle}>Meu Primeiro App</Text>
+        <View style={styles.card}>
+      <Text style={styles.title}>Hello World!</Text>
+        <Text style={styles.subtitle}>Minha primeira experiencia com React native</Text>
+      </View>
+      <StatusBar style="inverted" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+
   container: {
+    Color: '#fff' ,
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#2A2A2A',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  outtitle:{
+    fontSize: 22 , 
+    color: '#fff'
+  },
+
+  card:{
+    backgroundColor: '#fff' ,
+    padding: 12 ,
+    borderRadius: 15
+  },
+  title: {
+    fontSize: 22 ,
+    fontWeight: 'bold'
+  } ,
+  subtitle:{
+    color: '#999' ,
+    fontSize: 15
+  }
+
 });
